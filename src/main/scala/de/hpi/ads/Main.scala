607ee2902a77b9a713af9e769cf340c4787c4915
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-04-29 20:58:28 +0200

    Adds example code for actors

--- src/main/scala/de/hpi/ads/Main.scala
+++ src/main/scala/de/hpi/ads/Main.scala
@@ -1,7 +1,17 @@
 package de.hpi.ads
 
+import akka.actor.ActorSystem
+
 object Main {
     def main(args: Array[String]): Unit = {
-        printf("Hello World")
+        // create actor system
+        val actorSystem = ActorSystem("Actor Database System")
+
+        // create starting actors of the actor system
+//        val actor = actorSystem.actorOf(Props[Actor], name = "actor")
+//        val actorWithArgs = actorSystem.actorOf(Props(new Actor(arg1, arg2)), name = "actor")
+
+        // send a start message to actor(s)
+//        actor ! Message
     }
 }
