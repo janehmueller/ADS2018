48994d5e5c8765ecee633f4eccb373d97cbecca2
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-05-06 01:13:06 +0200

    Adds a few message calls to main

--- src/main/scala/de/hpi/ads/Main.scala
+++ src/main/scala/de/hpi/ads/Main.scala
@@ -1,66 +1,19 @@
 package de.hpi.ads
-import scala.util.{Failure, Success}
-import scala.concurrent.duration._
-import akka.actor.ActorSystem
-import akka.actor.ActorRef
-import akka.actor.Props
-import akka.pattern.{ask, pipe}
-import akka.util.Timeout
-import de.hpi.ads.actors.InterfaceActor.{CreateTableMessage, InsertRowMessage, SelectWhereMessage}
-import de.hpi.ads.actors._
-import de.hpi.ads.database.types.TableSchema
 
-import scala.collection.generic.CanBuildFrom
-import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import akka.actor.ActorSystem
+import de.hpi.ads.remote.actors._
+import de.hpi.ads.remote.actors.UserActor.{UserCreateTableMessage, UserInsertValuesMessage, UserSelectValuesMessage}
 
 object Main {
     def main(args: Array[String]): Unit = {
-        tests()
-    }
-
-    def tests() = {
-        val actorSystem = ActorSystem("Actor Database System")
-        val interfaceActor = actorSystem.actorOf(Props[InterfaceActor], name = "interfaceActor")
-        implicit val timeout = Timeout(5 seconds) // needed for `?` below
-
-        createTable(interfaceActor, "Movies", new TableSchema("id;title"))
-        insertValues(interfaceActor, "Movies", Array(Array(1, "movie1"), Array(2, "movie2")))
-        if (compareResults(selectValues(interfaceActor, "Movies", Array("title"), Array(("id", 1))), Array(Array("movie1")))) {
-            println("Test successful")
-        } else {
-            println("Test failed")
-        }
-    }
-
-    //creates one table
-    def createTable(actor: ActorRef, tableName: String, schema: TableSchema) = {
-        val createTableF: Future[Boolean] = (actor ? CreateTableMessage(tableName, schema)).mapTo[Boolean]
-        createTableF onComplete {
-            case Success(b) => println("Table " + tableName + " created successfully!")
-            case Failure(t) => println("An error has occurred during table creation: " + t.getMessage)
-        }
-    }
-
-    //inserts many rows into table
-    def insertValues(actor: ActorRef, tableName: String, data: Array[Array[Any]]) = {
-        val futures = (1 to data.length).map { i =>
-            actor ? InsertRowMessage(tableName, data(i))
-        }
-        Await.result(Future.sequence(futures), Duration.Inf)
-    }
-
-    //executes one select on table
-    def selectValues(actor: ActorRef, tableName: String, selectedColumns: Array[String], conditions: Array[String Tuple2 Any]) = {
-        val selectValF: Future[Array[Array[Any]]] = (actor ? SelectWhereMessage(tableName, selectedColumns, conditions)).mapTo
-        var res = Array[Array[Any]]()
-        selectValF onComplete {
-            case Success(result) => res = result
-            case Failure(t) => println("An error has occurred during value selection: " + t.getMessage)
-        }
-        res
-    }
-
-    def compareResults(result1: Array[Array[Any]] , result2: Array[Array[Any]]) = {
-        result1.deep == result2.deep
+        val actorSystem = ActorSystem("ActorDatabaseSystem")
+        val interfaceActor = actorSystem.actorOf(InterfaceActor.props(), InterfaceActor.defaultName)
+        val userActor = actorSystem.actorOf(UserActor.props(interfaceActor), UserActor.defaultName)
+        userActor ! UserCreateTableMessage("movies", "id;title")
+        userActor ! UserInsertValuesMessage("movies", List("1", "movie1"))
+        userActor ! UserInsertValuesMessage("movies", List("2", "movie2"))
+        userActor ! UserSelectValuesMessage("movies", List("title"), _.key == "1")
+        while (true) {}
+        actorSystem.terminate()
     }
 }
