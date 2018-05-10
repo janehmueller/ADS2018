83f237451fafabf121ad1a45274d975da5dd5427
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-05-10 17:27:11 +0200

    Adds query case class

--- src/main/scala/de/hpi/ads/remote/models/Query.scala
+++ src/main/scala/de/hpi/ads/remote/models/Query.scala
@@ -1,5 +1,18 @@
 package de.hpi.ads.remote.models
 
-class Query {
+import akka.actor.ActorRef
+import de.hpi.ads.database.Row
 
+case class Query(queryID: Int, receiver: ActorRef, var remainingResults: Int, var partialResult: List[Row] = Nil) {
+
+    def addPartialResult(resultRows: List[Row]): Unit = {
+        partialResult ++= resultRows
+        remainingResults -= 1
+    }
+
+    def isFinished: Boolean = remainingResults == 0
+}
+
+object Query {
+    def apply(queryID: Int, receiver: ActorRef): Query = Query(queryID, receiver, 1)
 }
