d035ef48be204a5ddf5d9d57a4c01087d5abc4dd
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-05-13 19:36:57 +0200

    Refactors InterfaceActor and adds missing CRUD messages

--- src/main/scala/de/hpi/ads/remote/actors/InterfaceActor.scala
+++ src/main/scala/de/hpi/ads/remote/actors/InterfaceActor.scala
@@ -1,11 +1,11 @@
 package de.hpi.ads.remote.actors
 
 import akka.actor.{ActorRef, Props}
-
-import de.hpi.ads.database.types.TableSchema
-import de.hpi.ads.remote.actors.TableActor.{TableInsertRowMessage, TableSelectWhereMessage}
-import de.hpi.ads.remote.actors.UserActor.TableCreationSuccessMessage
-import de.hpi.ads.remote.messages.QueryFailedMessage
+import de.hpi.ads.database.Row
+import de.hpi.ads.database.types.ColumnType
+import de.hpi.ads.remote.actors.TableActor._
+import de.hpi.ads.remote.actors.UserActor.{TableOpFailureMessage, TableOpSuccessMessage}
+import de.hpi.ads.remote.messages.{QueryFailureMessage, ShutdownMessage}
 
 import scala.collection.mutable.{Map => MMap}
 
@@ -19,71 +19,145 @@ object InterfaceActor {
         queryCounter - 1
     }
 
-    def props(): Props = Props(new InterfaceActor)
+    def props(resultCollector: ActorRef): Props = Props(new InterfaceActor(resultCollector))
 
-    case class CreateTableMessage(tableName: String, columnNames: List[String], columnDataTypes: List[Any], columnSizes: List[Int])
+    /** Table Management */
+    case class CreateTableMessage(tableName: String, schema: String)
+    case class CreateTableWithTypesMessage(tableName: String, columns: List[ColumnType])
+    case class DeleteTableMessage(tableName: String)
 
+    /** Table Create */
     case class InsertRowMessage(tableName: String, data: List[Any])
+    case class NamedInsertRowMessage(tableName: String, data: List[(String, Any)])
 
-    case class SelectWhereMessage(tableName: String,
-                                  projection: List[String],
-                                  conditionColumnNames: List[String],
-                                  conditionOperators: List[String],
-                                  conditionValues: List[Any])
+    // TODO conditions in a format that can use indices
+    /** Table Read */
+    case class SelectWhereMessage(tableName: String, projection: List[String], conditions: Row => Boolean)
+
+    /** Table Update */
+    case class UpdateWhereMessage(tableName: String, data: List[(String, Any)], conditions: Row => Boolean)
+
+    /** Table Delete */
+    case class DeleteWhereMessage(tableName: String, conditions: Row => Boolean)
 }
 
-class InterfaceActor extends ADSActor {
+class InterfaceActor(resultCollector: ActorRef) extends ADSActor {
     import InterfaceActor._
 
     val tables: MMap[String, ActorRef] = MMap.empty
 
-    def receive: Receive = {
-        case CreateTableMessage(tableName, columnNames, columnDataTypes, columnSizes) => createTable(tableName, columnNames, columnDataTypes, columnSizes)
-        case InsertRowMessage(table, data) => insertRow(table, data)
-        case SelectWhereMessage(table, projection, conditionColNames, conditionOps, conditionVals) => selectWhere(table, projection, conditionColNames, conditionOps, conditionVals)
-        case default => log.error(s"Received unknown message: $default")
-    }
-
-    def createTable(tableName: String, columnNames: List[String], columnDataTypes: List[Any], columnSizes: List[Int]): Unit = {
-        if (assertTableExistance(tableName, negateCheck = true)) {
-            return
-        }
-        val tableActor = this.context.actorOf(TableActor.props(tableName, fileName(tableName), new TableSchema(columnNames, columnDataTypes, columnSizes)), actorName(tableName))
-        tables(tableName) = tableActor
-        this.sender() ! TableCreationSuccessMessage(tableName)
-    }
-
-    def insertRow(tableName: String, data: List[Any]): Unit = {
-        if (assertTableExistance(tableName)) {
-            return
-        }
-        val tableActor = tables(tableName)
-        tableActor ! TableInsertRowMessage(data.map(_.toString), this.sender())
-    }
-
-    def selectWhere(tableName: String,
-                    projection: List[String],
-                    conditionColumnNames: List[String],
-                    conditionOperators: List[String],
-                    conditionValues: List[Any]): Unit = {
-        if (assertTableExistance(tableName)) {
-            return
-        }
-        val tableActor = tables(tableName)
-        tableActor ! TableSelectWhereMessage(nextQueryId, projection, conditionColumnNames, conditionOperators, conditionValues, this.sender())
-    }
-
     def assertTableExistance(tableName: String, negateCheck: Boolean = false): Boolean = {
         val exists = tables.contains(tableName)
         if (!exists && !negateCheck) {
-            this.sender() ! QueryFailedMessage(-1, s"Table $tableName does not exist!")
+            this.sender() ! QueryFailureMessage(-1, s"Table $tableName does not exist!")
         } else if (exists && negateCheck) {
-            this.sender() ! QueryFailedMessage(-1, s"Table $tableName already exists!")
+            this.sender() ! QueryFailureMessage(-1, s"Table $tableName already exists!")
         }
         !(exists ^ negateCheck)
     }
 
-    def actorName(tableName: String): String = s"TABLE_${tableName.toUpperCase}"
+    def tableExists(tableName: String): Boolean = tables.contains(tableName)
 
-    def fileName(tableName: String): String = s"table.$tableName.ads"
+    def receive: Receive = {
+        /** Table Management */
+        case CreateTableMessage(tableName, schema) => createTable(tableName, schema)
+        case CreateTableWithTypesMessage(tableName, columns) => createTable(tableName, columns)
+        case DeleteTableMessage(tableName) => deleteTable(tableName)
+
+        /** Table Create */
+        case InsertRowMessage(tableName, data) => insertRow(tableName, data)
+        case NamedInsertRowMessage(tableName, data) => insertRowWithNames(tableName, data)
+
+        /** Table Read */
+        case SelectWhereMessage(tableName, projection, conditions) => selectWhere(tableName, projection, conditions)
+
+        /** Table Update */
+        case UpdateWhereMessage(tableName, projection, conditions) => updateWhere(tableName, projection, conditions)
+
+        /** Table Delete */
+        case DeleteWhereMessage(tableName, conditions) => deleteWhere(tableName, conditions)
+
+        /** Default case */
+        case default => log.error(s"Received unknown message: $default")
+    }
+
+    def createTable(tableName: String, schema: String): Unit = {
+        if (tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "CREATE", "Table already exists!")
+            return
+        }
+        val tableActor = this.context.actorOf(
+            TableActor.props(tableName, TableActor.fileName(tableName), schema, resultCollector), TableActor.actorName(tableName)
+        )
+        tables(tableName) = tableActor
+        this.sender ! TableOpSuccessMessage(tableName, "CREATE")
+    }
+
+    def createTable(tableName: String, columns: List[ColumnType]): Unit = {
+        if (tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "CREATE", "Table already exists!")
+            return
+        }
+        val tableActor = this.context.actorOf(
+            TableActor.props(tableName, TableActor.fileName(tableName), columns, resultCollector), TableActor.actorName(tableName)
+        )
+        tables(tableName) = tableActor
+        this.sender ! TableOpSuccessMessage(tableName, "CREATE")
+    }
+
+    def deleteTable(tableName: String): Unit = {
+        if (!tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "DROP", "Table does not exist!")
+            return
+        }
+        val tableActor = tables.remove(tableName).get
+//        context.stop(tableActor)
+        tableActor ! ShutdownMessage
+        this.sender ! TableOpSuccessMessage(tableName, "DROP")
+    }
+
+    def insertRow(tableName: String, data: List[Any]): Unit = {
+        if (!tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "INSERT", "Table does not exist!")
+            return
+        }
+        val tableActor = tables(tableName)
+        tableActor ! TableInsertRowMessage(nextQueryId, data, this.sender())
+    }
+
+    def insertRowWithNames(tableName: String, data: List[(String, Any)]): Unit = {
+        if (!tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "INSERT", "Table does not exist!")
+            return
+        }
+        val tableActor = tables(tableName)
+        tableActor ! TableNamedInsertRowMessage(nextQueryId, data, this.sender())
+    }
+
+    def selectWhere(tableName: String, projection: List[String], conditions: Row => Boolean): Unit = {
+        if (!tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "SELECT", "Table does not exist!")
+            return
+        }
+        val tableActor = tables(tableName)
+        tableActor ! TableSelectWhereMessage(nextQueryId, projection, conditions, this.sender())
+    }
+
+    def updateWhere(tableName: String, data: List[(String, Any)], conditions: Row => Boolean): Unit = {
+        if (!tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "UPDATE", "Table does not exist!")
+            return
+        }
+        val tableActor = tables(tableName)
+        tableActor ! TableUpdateWhereMessage(nextQueryId, data, conditions, this.sender())
+    }
+
+    def deleteWhere(tableName: String, conditions: Row => Boolean): Unit = {
+        if (!tableExists(tableName)) {
+            this.sender() ! TableOpFailureMessage(tableName, "DELETE", "Table does not exist!")
+            return
+        }
+        val tableActor = tables(tableName)
+        tableActor ! TableDeleteWhereMessage(nextQueryId, conditions, this.sender())
+    }
 }
