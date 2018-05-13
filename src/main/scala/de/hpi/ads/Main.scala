8be44596e4368e916ac50bc8a0a05592157eb146
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2018-05-13 19:48:47 +0200

    Adds example inserts and updates in main

--- src/main/scala/de/hpi/ads/Main.scala
+++ src/main/scala/de/hpi/ads/Main.scala
@@ -1,17 +1,47 @@
 package de.hpi.ads
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
+import de.hpi.ads.database.types._
+import de.hpi.ads.remote.actors.InterfaceActor._
+import de.hpi.ads.remote.actors.UserActor.ExecuteCommandMessage
 import de.hpi.ads.remote.actors._
-import de.hpi.ads.remote.actors.UserActor.{UserCreateTableMessage, UserInsertValuesMessage, UserSelectValuesMessage}
 
 object Main {
     def main(args: Array[String]): Unit = {
         val actorSystem = ActorSystem("ActorDatabaseSystem")
-        val interfaceActor = actorSystem.actorOf(InterfaceActor.props(), InterfaceActor.defaultName)
+        val resultCollector = actorSystem.actorOf(ResultCollectorActor.props(), ResultCollectorActor.defaultName)
+        val interfaceActor = actorSystem.actorOf(InterfaceActor.props(resultCollector), InterfaceActor.defaultName)
         val userActor = actorSystem.actorOf(UserActor.props(interfaceActor), UserActor.defaultName)
-        userActor ! UserCreateTableMessage("movies", List("id", "title"), List(Int, "S"), List(0, 255))
-        userActor ! UserInsertValuesMessage("movies", List("1", "movie1"))
-        userActor ! UserInsertValuesMessage("movies", List("2", "movie2"))
+        userActor ! ExecuteCommandMessage(CreateTableMessage("actors", "id;name;surname"))
+        userActor ! ExecuteCommandMessage(InsertRowMessage("actors", List("1", "Max", "Mustermann")))
+        userActor ! ExecuteCommandMessage(InsertRowMessage("actors", List("2", "Max", "Metermann")))
+        userActor ! ExecuteCommandMessage(SelectWhereMessage("actors", List("id", "name", "surname"), _.getByName("name") == "Max"))
+        val movieTypes = List(
+            ColumnType("id", IntType),
+            ColumnType("name", StringType),
+            ColumnType("actors", ListType(StringType)),
+            ColumnType("rating", DoubleType)
+        )
+        userActor ! ExecuteCommandMessage(CreateTableWithTypesMessage("movies", movieTypes))
+        val row = List(
+            ("id", 1),
+            ("name", "Ready"),
+            ("actors", List("actor 1", "actor 2", "actor 3")),
+            ("rating", 3.5)
+        )
+        userActor ! ExecuteCommandMessage(NamedInsertRowMessage("movies", row))
+        userActor ! ExecuteCommandMessage(SelectWhereMessage("movies", List("id", "name", "actors", "rating"), _.key == 1))
+        userActor ! ExecuteCommandMessage(UpdateWhereMessage("movies", List(("name", "Ready Player One"), ("rating", 9.5)), _.key == 1))
+        userActor ! ExecuteCommandMessage(SelectWhereMessage("movies", List("id", "name", "actors", "rating"), _.key == 1))
+        while (true) {}
         actorSystem.terminate()
+    }
+
+    def startActorSystem(): ActorRef = {
+        val actorSystem = ActorSystem("ActorDatabaseSystem")
+        val resultCollector = actorSystem.actorOf(ResultCollectorActor.props(), ResultCollectorActor.defaultName)
+        val interfaceActor = actorSystem.actorOf(InterfaceActor.props(resultCollector), InterfaceActor.defaultName)
+        val userActor = actorSystem.actorOf(UserActor.props(interfaceActor), UserActor.defaultName)
+        userActor
     }
 }
